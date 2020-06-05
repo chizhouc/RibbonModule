8ed116c89fdda9d07d3c667d576f1109338c209e
chizhouc <13545862545@163.com>
2020-06-05 23:14:07 +0800

    提交

---
+++ ribbonserverdemo/src/main/java/com/example/ribbonserverdemo/RibbonserverdemoApplication.java
@@ -0,0 +1,13 @@
+package com.example.ribbonserverdemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RibbonserverdemoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(RibbonserverdemoApplication.class, args);
+    }
+
+}
---
+++ ribbonserverdemo/src/test/java/com/example/ribbonserverdemo/RibbonserverdemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.ribbonserverdemo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RibbonserverdemoApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
