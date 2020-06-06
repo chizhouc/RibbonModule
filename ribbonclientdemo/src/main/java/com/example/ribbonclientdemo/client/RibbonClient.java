dc7b63bce76290bdbc40bc5fc8c9a7c3e9641df5
chizhouc <13545862545@163.com>
2020-06-06 10:07:48 +0800

    提交

--- ribbonclientdemo/src/main/java/com/example/ribbonclientdemo/client/RibbonClient.java
+++ ribbonclientdemo/src/main/java/com/example/ribbonclientdemo/client/RibbonClient.java
@@ -1,7 +1,11 @@
 package com.example.ribbonclientdemo.client;
 
+import com.netflix.config.ConfigurationManager;
+
 public class RibbonClient {
     public static void main(String[] args) {
+        ConfigurationManager.getConfigInstance().setProperty(
+                "sample-client.ribbon.listOfServers", "www.linkedin.com:80,www.google.com:80");
 
     }
 }
